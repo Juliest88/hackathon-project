33a2ce0582e3a3d9e12465b4ea49c056a2eca07e
Julia <julia.s@webpals.com>
2019-07-28 22:25:29 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChartModule } from 'angular-highcharts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartColumnComponent } from './components/chart/chart-column/chart-column.component';
 import { ChartComponent } from './components/chart/chart.component';
+import * as Highcharts from 'highcharts/highcharts';
+import * as HighchartsMore from 'highcharts/highcharts-more';
+import * as HighchartsSolidGauge from 'highcharts/modules/solid-gauge';
 
+// Now init modules:
+(<any>HighchartsMore)(Highcharts);
+(<any>HighchartsSolidGauge)(Highcharts);
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +22,8 @@ import { ChartComponent } from './components/chart/chart.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/chart/chart-column/chart-column.component.ts
+++ src/app/components/chart/chart-column/chart-column.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Team } from 'src/app/shared/models/Team';
+import { Chart } from 'angular-highcharts';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'chart-column',
@@ -9,10 +11,11 @@ import { Team } from 'src/app/shared/models/Team';
 export class ChartColumnComponent implements OnInit {
   @Input() chartItem: Team;
   @ViewChild('performanceBar', { static: true }) performanceBar: ElementRef;
-
+  pervasivenessScoreGaugeOptions:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.initBotScore(50)
     this.setColumnHeightPercentage(0);
     setTimeout(() => {
       this.setColumnHeightPercentage(this.chartItem.completion_rate);
@@ -21,6 +24,93 @@ export class ChartColumnComponent implements OnInit {
 
   setColumnHeightPercentage(height: number) {
     this.performanceBar.nativeElement.style.height = height + '%';
+  }
+
+  private getGaugeBaseOptions(value: number):any {
+    return {
+      chart: {
+        height: 200,
+        type: 'solidgauge',
+        margin: [0, 0, 0, 0],
+        backgroundColor: 'transparent'
+      },
+      title: null,
+      yAxis: {
+        stops: [
+          [0.4, '#55BF3B'], // green
+          [0.7, '#DDDF0D'], // yellow
+          [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        tickWidth: 0,
+        minorTickInterval: null,
+        labels: {
+          enabled: false
+        }
+      },
+      pane: {
+        size: '85%',
+        center: ['50%', '85%'],
+        startAngle: -100,
+        endAngle: 100,
+        background: {
+          borderWidth: 13,
+          backgroundColor: '#DBDBDB',
+          shape: 'arc',
+          borderColor: '#2219a5',
+          outerRadius: '80%',
+          innerRadius: '100%'
+        }
+      },
+      tooltip: {
+        enabled: false
+      },
+      plotOptions: {
+        solidgauge: {
+          borderColor: this.chartItem.bgColor,
+          borderWidth: 13,
+          radius: 90,
+          innerRadius: '100%',
+          dataLabels: {
+            y: 5,
+            borderWidth: 0,
+            useHTML: true
+          }
+        }
+      }
+    };
+  }
+
+  initBotScore(score) {
+    let options:any = Highcharts.merge(
+      this.getGaugeBaseOptions(score),
+      {
+        yAxis: {
+          min: 0,
+          max: 100,
+          title: {
+            text: ''
+          }
+        },
+
+        credits: {
+          enabled: false
+        },
+
+        series: [
+          {
+            data: [score],
+            dataLabels: {
+              format: `<div class='score-value'><span>{y}</span></div>`
+            },
+            tooltip: {
+              valueSuffix: 'year revenue'
+            }
+          }
+        ]
+      }
+    );
+    this.pervasivenessScoreGaugeOptions = new Chart(options);
   }
 
 }
